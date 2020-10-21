8284ea33cd8c50939591ba47950dc8dfa0ae63f0
Mariia Kosareva <Mariia_Kosareva@epam.com>
2020-10-21 23:16:54 +0300

    task 2

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -10,7 +10,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        My Store
+        {`*** ʕ•ᴥ•ʔ ***`} 
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
